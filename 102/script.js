9e909be569cd86e6eecfd1f57c64f5d653d11e89
laaouatni <laaouatni.anas@outlook.it>
2022-01-09 01:00:24 +0100

    102

---
+++ 102/script.js
@@ -0,0 +1,14 @@
+/* function display_elt(elt_class, time) {
+    setTimeout(function() {
+        document.querySelector(elt_class);
+    }, time)
+}
+
+var time_int = 0;
+divs = ['elt1', 'elt2', 'elt3'];
+
+for (let i = 0; i < 4; i++) {
+    time_int += 0.5;
+
+    function display_elt(elt_class[i], time_int);
+} */
