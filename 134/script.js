a4db0c2f1c062e45ded77e27db4924112d7b5e51
laaouatni <laaouatni.anas@outlook.it>
2022-06-26 23:05:19 +0200

    133 answer (not completed) StackOverFlow - laaouatni

---
+++ 134/script.js
@@ -0,0 +1,51 @@
+/* let rock = document.querySelector(".rock-btn");
+let paper = document.querySelector(".paper-btn");
+let scissors = document.querySelector(".scissors-btn");
+
+let select = document.querySelector(".select");
+let computerChoice = document.querySelector(".comp-choice");
+let result = document.querySelector(".result");
+
+let allChoices = ["Rock", "Paper", "Scissors"];
+let allButtons = [rock, paper, scissors];
+
+let allResults = [{
+    message: "Result: Congratulations, You won!",
+    color: "#1e612b"
+}, {
+    message: "Result: It's a draw!",
+    color: "#5c3000"
+}, {
+    message: "Result: Oh no, You lost!",
+    color: "#8d1d1d"
+}]
+
+allButtons.forEach((choice, index) => {
+    choice.addEventListener("click", () => {
+        let randomNum = Math.floor(Math.random() * 3);
+        let compChoice = allChoices[randomNum];
+        let userChoice = allChoices[index];
+
+        console.log(userChoice)
+
+        select.innerHTML = `select: ${userChoice}`;
+        result.style.removeProperty("color");
+        computerChoice.innerHTML = `Computer choose: ${compChoice}`;
+
+        if (compChoice == userChoice) {
+            result.innerHTML = allResults[1].message;
+            result.style.color = allResults[1].color;
+        }
+
+        if (compChoice == "Rock") {
+            result.innerHTML = "Result: It's a draw!";
+            result.style.color = "#5c3000";
+        } else if (compChoice == "Paper") {
+            result.innerHTML = "Result: Oh no, You lost!";
+            result.style.color = "#8d1d1d";
+        } else if (compChoice == "Scissors") {
+            result.innerHTML = "Result: Congratulations, You won!";
+            result.style.color = "#1e612b";
+        }
+    });
+}); */
