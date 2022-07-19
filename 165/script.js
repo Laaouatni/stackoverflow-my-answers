953524692b1b37ad4abfcc4b63009b54bda19194
laaouatni <laaouatni.anas@outlook.it>
2022-07-19 19:01:47 +0200

    165 (not finished completely)

--- 165/script.js
+++ 165/script.js
@@ -1,51 +1,54 @@
 let TextArray = ["the", "dev", "ninja"];
+createNavWA();
 
-let navContainer = document.querySelector("nav");
-let bodyElement = document.querySelector("body");
-
-bodyElement.style.setProperty("--total-nav-items", `${TextArray.length}`);
-
-createUiWithForLoop();
-makeDefaultActiveItem();
-
-function createUiWithForLoop() {
-    TextArray.forEach((thisText, index) => {
-        let thisNavItem;
-        createNavItem();
-        appendChildToNav();
+function createNavWA() {
+    let navContainer = document.querySelector("nav");
+    let bodyElement = document.querySelector("body");
+    
+    bodyElement.style.setProperty("--total-nav-items", `${TextArray.length}`);
+    
+    createUiWithForLoop();
+    makeDefaultActiveItem();
+    
+    function createUiWithForLoop() {
+        TextArray.forEach((thisText, index) => {
+            let thisNavItem;
+            createNavItem();
+            appendChildToNav();
+            
+            thisNavItem.addEventListener("click", changeActiveIndex);
         
-        thisNavItem.addEventListener("click", changeActiveIndex);
-    
-        function changeActiveIndex() {
-            bodyElement.style.setProperty("--active-index", `${index}`);
-            addActiveClassToItem();
-    
-            function addActiveClassToItem() {
-                let allNavItems = document.querySelectorAll(".nav-item");
-    
-                allNavItems.forEach((item) => {
-                    item.classList.remove("active");
-                });
-    
-                thisNavItem.classList.add("active");
+            function changeActiveIndex() {
+                bodyElement.style.setProperty("--active-index", `${index}`);
+                addActiveClassToItem();
+        
+                function addActiveClassToItem() {
+                    let allNavItems = document.querySelectorAll(".nav-item");
+        
+                    allNavItems.forEach((item) => {
+                        item.classList.remove("active");
+                    });
+        
+                    thisNavItem.classList.add("active");
+                }
             }
-        }
+        
+            function createNavItem() {
+                thisNavItem = document.createElement("div");
+                thisNavItem.classList.add("nav-item");
+                thisNavItem.textContent = `${thisText}`;
+            }
+        
+            function appendChildToNav() {
+                navContainer.appendChild(thisNavItem);
+            }
+        });
+    }
     
-        function createNavItem() {
-            thisNavItem = document.createElement("div");
-            thisNavItem.classList.add("nav-item");
-            thisNavItem.textContent = `${thisText}`;
-        }
+    function makeDefaultActiveItem() {
+        let allNavItems = document.querySelectorAll(".nav-item");
+        let indexActiveNow = getComputedStyle(bodyElement).getPropertyValue("--active-index").replace(" ", "");
     
-        function appendChildToNav() {
-            navContainer.appendChild(thisNavItem);
-        }
-    });
-}
-
-function makeDefaultActiveItem() {
-    let allNavItems = document.querySelectorAll(".nav-item");
-    let indexActiveNow = getComputedStyle(bodyElement).getPropertyValue("--active-index").replace(" ", "");
-
-    allNavItems[indexActiveNow].classList.add("active");
+        allNavItems[indexActiveNow].classList.add("active");
+    }
 }
