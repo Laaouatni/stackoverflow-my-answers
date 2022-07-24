ba8623a52747dabbcf1104910568eb68abb654fd
laaouatni <laaouatni.anas@outlook.it>
2022-07-24 16:38:27 +0200

    183 beta

--- 183/script.js
+++ 183/script.js
@@ -1,10 +1,9 @@
-let bodyElement = document.querySelector("body");
 let navItems = document.querySelectorAll(".nav-item");
 
-bodyElement.style.setProperty("--total-nav-items", `${navItems.length}`);
+document.body.style.setProperty("--total-nav-items", `${navItems.length}`);
 
 navItems.forEach((item, index) => {
 	item.addEventListener("click", () => {
-		bodyElement.style.setProperty("--index", `${index}`);
+    document.body.style.setProperty("--index", `${index}`);
 	});
 });
