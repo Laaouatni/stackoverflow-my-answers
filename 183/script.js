b93a3001a3adc57d29f30e66cd750cc1e53ce063
laaouatni <laaouatni.anas@outlook.it>
2022-07-25 00:22:49 +0200

    whatsapp navbar clone finished

--- 183/script.js
+++ 183/script.js
@@ -4,6 +4,14 @@ document.body.style.setProperty("--total-nav-items", `${navItems.length}`);
 
 navItems.forEach((item, index) => {
 	item.addEventListener("click", () => {
-    document.body.style.setProperty("--index", `${index}`);
+		document.body.style.setProperty("--index", `${index}`);
+		toogleActive();
+
+		function toogleActive() {
+			navItems.forEach((itemWithClass) => {
+				itemWithClass.classList.remove("active");
+			});
+			item.classList.add("active");
+		}
 	});
 });
