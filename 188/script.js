b82a1f21096327c09f978ae16f744f1ddc372252
laaouatni <laaouatni.anas@outlook.it>
2022-07-29 22:24:02 +0200

    188

---
+++ 188/script.js
@@ -0,0 +1,8 @@
+let metaString = `
+		<meta charset="UTF-8" />
+		<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+		<title>my repetitive title </title>
+`;
+
+document.head.innerHTML += metaString;
