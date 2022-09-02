995d354df52a47c7943c468692512bd6c6c1d2a2
laaouatni <laaouatni.anas@outlook.it>
2022-09-02 15:37:09 +0200

    223

--- 223/script.js
+++ 223/script.js
@@ -3,17 +3,24 @@ let langs = ["html", "css", "javascript"];
 
 let outputs = document.querySelectorAll("code");
 
-inputs.forEach((input, index) => {
-  input.value = localStorage.getItem(`${langs[index]}`) || "";
-  updateCode(input, index);
+outputs.forEach((code, index) => {
+  // ✅ make it like input 
+  code.contentEditable = "true";
+  // ✅ if there is some code from localStorage we add it to the website.
+  code.innerHTML = localStorage.getItem(`${langs[index]}`) || "";
+  // ✅ styling of your library (work great when app start)
+  hljs.highlightElement(code);
 
-  input.addEventListener("input", () => {
-    localStorage.setItem(`${langs[index]}`, input.value);
-    updateCode(input, index);
+  code.addEventListener("input", () => {
+    // console.log("first",  code.innerHTML)
+    localStorage.setItem(`${langs[index]}`, code.innerHTML);
+    // ❌ why the typing is reversed here?
+    // if the next line isn't there then it work fine, otherwise it will change the values to reversed
+    hljs.highlightElement(code);
+    // console.log("after", code.innerHTML)
+
+    // console.log
+    // example to type <div></div>
+    // what I get >vid/<>vid<
   });
 });
-
-function updateCode(input, index) {
-  outputs[index].textContent = input.value;
-  hljs.highlightElement(outputs[index]);
-}
