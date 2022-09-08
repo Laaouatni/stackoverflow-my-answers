0fb609180f0cca661dadfcefaabb9c99d4c1d491
laaouatni <laaouatni.anas@outlook.it>
2022-09-08 22:31:27 +0200

    231

--- 231/script.js
+++ 231/script.js
@@ -1,3 +1,13 @@
-console.log(
-  (12330 / 100).toFixed(2)
-);
+function convert(value) {
+  if (value % 1 === 0) {
+    return value;
+  }
+
+  return value.toFixed(2);
+}
+
+console.log(convert(12330 / 100)); // should return value with toFixed
+
+console.log(convert(100 / 100)); // should return 1 (no toFixed)
+
+console.log(convert(100 / 10)); // should return 10 (no toFixed)
