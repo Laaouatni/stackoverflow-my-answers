7263603f349bdd66d3131d625fc3ea2f3c61a79f
laaouatni <laaouatni.anas@outlook.it>
2022-10-02 16:18:28 +0200

    246

---
+++ 246/script.js
@@ -0,0 +1,28 @@
+class Coords {
+  static array = [];
+
+  static translate(dx = 0, dy = 0) {
+    Coords.array.forEach((point) => {
+      point.x += dx;
+      point.y += dy;
+    });
+  }
+
+  static rotate(angleDeg) {
+    const angleRad = (angleDeg * Math.PI) / 180;
+    Coords.array.forEach((point) => {
+      point.x = point.x * Math.cos(angleRad) - point.y * Math.sin(angleRad);
+      point.y = point.x * Math.sin(angleRad) + point.y * Math.cos(angleRad);
+    });
+  }
+}
+
+class Point extends Coords {
+  constructor(x, y) {
+    super();
+    this.x = x;
+    this.y = y;
+
+    Coords.array.push(this);
+  }
+}
