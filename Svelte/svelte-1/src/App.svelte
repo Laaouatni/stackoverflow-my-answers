2c82480e3b0a3e23e0ef2bf6249f40e819061069
laaouatni <laaouatni.anas@outlook.it>
2022-11-26 20:02:40 +0100

    svelte 1 stackoverflow answer

---
+++ Svelte/svelte-1/src/App.svelte
@@ -0,0 +1,15 @@
+<script>
+  let objs = []
+  
+  function update() {
+    objs = [...objs, "a"];
+  }
+</script>
+
+<h1>Lists</h1>
+
+{#each objs as obj}
+  <div>{obj}</div>
+{/each}
+
+<button on:click={update}>Update</button>
